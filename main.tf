dfb43f0b32ce7b8951332548633ae252ddb4839a
Azad parinde <azadparinde00@gmail.com>
2025-09-05 15:22:57 +0530

    update main file with resource group creation

--- main.tf
+++ main.tf
@@ -11,22 +11,27 @@ terraform {
        features {}
      }
      
+     resource "azurerm_resource_group" "main" {
+       name     = var.resource_group_name
+       location = var.location
+     }
+     
      module "vnet" {
        source              = "./Vnet"
-       resource_group_name = var.resource_group_name
-       location            = var.location
+       resource_group_name = azurerm_resource_group.main.name
+       location            = azurerm_resource_group.main.location
      }
      
      module "acr" {
        source              = "./acr"
-       resource_group_name = var.resource_group_name
-       location            = var.location
+       resource_group_name = azurerm_resource_group.main.name
+       location            = azurerm_resource_group.main.location
      }
      
      module "aks" {
        source              = "./aks"
-       resource_group_name = var.resource_group_name
-       location            = var.location
+       resource_group_name = azurerm_resource_group.main.name
+       location            = azurerm_resource_group.main.location
        subnet_id           = module.vnet.subnet_id
        acr_login_server    = module.acr.acr_login_server
      }
